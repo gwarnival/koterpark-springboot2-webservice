5cb87efaa5b557e56c196f8dd9254502cfb88e3f
Kenny <kkh900629@gmail.com>
2021-05-30 23:27:59 +0900

    JPA update 기능 추가

--- src/main/java/org/koterpark/web/springboot/service/PostsService.java
+++ src/main/java/org/koterpark/web/springboot/service/PostsService.java
@@ -1,8 +1,11 @@
 package org.koterpark.web.springboot.service;
 
 import lombok.RequiredArgsConstructor;
+import org.koterpark.web.springboot.domain.posts.Posts;
 import org.koterpark.web.springboot.domain.posts.PostsRepository;
+import org.koterpark.web.springboot.web.dto.PostsResponseDto;
 import org.koterpark.web.springboot.web.dto.PostsSaveRequestDto;
+import org.koterpark.web.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,5 +17,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/test/java/org/koterpark/web/springboot/web/PostsApiControllerTest.java
+++ src/test/java/org/koterpark/web/springboot/web/PostsApiControllerTest.java
@@ -6,10 +6,13 @@ import org.junit.runner.RunWith;
 import org.koterpark.web.springboot.domain.posts.Posts;
 import org.koterpark.web.springboot.domain.posts.PostsRepository;
 import org.koterpark.web.springboot.web.dto.PostsSaveRequestDto;
+import org.koterpark.web.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -57,4 +60,41 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getContent()).isEqualTo(content);
 
     }
+
+    @Test
+    public void Posts_수정된다() {
+
+        Posts savePosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long updateId = savePosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate
+                .exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+
+
+
+    }
+
 }
