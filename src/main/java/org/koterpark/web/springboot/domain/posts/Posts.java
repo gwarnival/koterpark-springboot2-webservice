c5d7edd7549df160b5a52e5c8ac4e2844aae6d9b
Kenny <kkh900629@gmail.com>
2021-06-01 00:00:33 +0900

    JPA BaseTimeEntity 이용해서 등록,수정일 자동 기록 추가

--- src/main/java/org/koterpark/web/springboot/domain/posts/Posts.java
+++ src/main/java/org/koterpark/web/springboot/domain/posts/Posts.java
@@ -3,13 +3,14 @@ package org.koterpark.web.springboot.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.koterpark.web.springboot.domain.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
